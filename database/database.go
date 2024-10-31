6fde949f41839ef23a8abc926e9544efe0634138
Hossein Araghi <hoseinaraghi84@gmail.com>
2024-10-31 19:42:22 +0330

    feat(database):  Implement database connection to handle queries and models

---
+++ database/database.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"telegram_todo_bot/config"
+	"telegram_todo_bot/models"
+
+	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Init() *gorm.DB {
+	// Define database connection info
+	dsn := fmt.Sprintf("host=%s user=%s password=%s port=%s, dbname=%s",
+		config.Config("POSTGRES_HOST"),
+		config.Config("POSTGRES_USER"),
+		config.Config("POSTGRES_PASSWORD"),
+		config.Config("POSTGRES_PORT"),
+		"postgres")
+
+	// Connect to database
+	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Remove old database
+	//if err := DB.Exec("DROP DATABASE IF EXISTS todolist;").Error; err != nil {
+	//	panic(err)
+	//}
+
+	// Create database
+	if err := DB.Exec("CREATE DATABASE IF NOT EXISTS todolist").Error; err != nil {
+		panic(err)
+	}
+
+	// Migrate tables
+	DB.AutoMigrate(&models.Task{})
+
+	return DB
+}
